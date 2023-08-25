1f7f1472f60baf0d3025eafc23eac15d2283c325
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-08-24 20:32:01 -0400

    added simple callback message handler

--- routes/api/callback.js
+++ routes/api/callback.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 const router = new Router();
 
+import { createMessage } from "../../controllers/callbackControllers.js";
+
+router.route("/").post(createMessage);
+
 // TODO: Implement callback routes
 
 export default router;
