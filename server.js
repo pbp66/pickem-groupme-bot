6deac5d746a96280ba2099aa7908101f45dc2471
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-08-24 20:05:53 -0400

    removed additional express.listen call

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-	console.log(`API server for ${activity} running on port ${PORT}!`);
-});
-
 db.once("open", () => {
 	app.listen(PORT, () => {
 		console.log(`API server for ${activity} running on port ${PORT}!`);
