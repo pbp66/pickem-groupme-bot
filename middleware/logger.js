4ba344792a6fca7d945c7892bddbb33700f32f9b
James Perry <pbp66.coding@gmail.com>
2023-09-27 16:25:58 -0400

    added logger todos

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,8 @@
 const logger = (req, res, next) => {
 	console.log(`\n${req.method} request hit path ${req.url}\n`);
 	console.log(req.body);
+	// TODO: Update logger to be more concise yet provide more info for incoming messages
+	// TODO: create individual loggers for different paths? API, Callbacks, etc?
 	next();
 };
 
