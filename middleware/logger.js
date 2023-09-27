1dd3256aab39116bb18c047e815319cd5086e5a4
James Perry <pbp66.coding@gmail.com>
2023-09-27 16:22:17 -0400

    removed chalk. colored loggin not applicable to heroku

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,12 +1,6 @@
-/* Credit to Josh Naylor for this simple logger function from class*/
-import chalk from "chalk";
-
 const logger = (req, res, next) => {
-	console.log(
-		chalk.black.bgBlue.bold(`\n${req.method} request hit path ${req.url}\n`)
-	);
+	console.log(`\n${req.method} request hit path ${req.url}\n`);
 	console.log(req.body);
-
 	next();
 };
 
